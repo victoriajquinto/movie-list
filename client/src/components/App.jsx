06f2f70e5478e409a44edc4b41697dc3eb0dab9d
victoriajquinto <victoriajquinto@gmail.com>
2023-06-01 18:08:42 -0700

    save progress in Level 2

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,71 @@
 import React from 'react';
+import { useState } from 'react';
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+const MovieList = ({ movies, searchText, newMovies }) => {
+  //map movie list array, pass title to MovieFormat
+  // <MovieFormat movie = {movie} key = {i}/>
+  let list;
+
+  if(newMovies.length !== 0) {
+    list = newMovies.map((movie, i) => <MovieFormat movie = {movie} key = {i}/>)
+  } else {
+    list = movies
+      .filter((movie)=>{return movie.title.toLowerCase().indexOf(searchText.toLowerCase()) !== -1})
+      .map((movie, i) => <MovieFormat movie = {movie} key = {i}/>)
+  }
+
+
+  if(!list.length) {
+    list = 'No matches found, sorry!'
+  }
+  return (<>{list}</>)
+}
+
+const MovieFormat = ({movie: {title}}) => (
+  <div>{title}</div>
+)
+
+const SearchBar = ({searchText, setSearchText}) => ( //{ event } ??
+  <>
+    <form name="searchComponent" onSubmit={(event) => { event.preventDefault(); return (setSearchText(event.target.value))}}>
+      <input type="search" name="movie" id="searchText" defaultValue={searchText} placeholder="Search..." ></input>
+      <input type="submit" value="Go!" ></input>
+    </form>
+  </>
+)
+
+const NewMovies = ({newMovies, setNewMovies, addNewMovies, movies}) => {
+
+  return (
+    <>
+      <form name="newMoviesComponent">
+        <input type="text" name="suggestion" id="addText" defaultValue={newMovies} placeholder="Add movie title here" onChange={(event) => setNewMovies(event.target.value)}></input>
+        <input type="submit" value="Add!" ></input>
+      </form>
+    </>
+  )
+}
+
+const App = () => {
+  const [searchText, setSearchText] = useState('');
+  const [newMovies, setNewMovies] = useState([]);
+  var movies = [
+    {title: 'Mean Girls'},
+    {title: 'Hackers'},
+    {title: 'The Grey'},
+    {title: 'Sunshine'},
+    {title: 'Ex Machina'},
+  ];
+
+  return (
+  <div>
+    <NewMovies newMovies={newMovies} setNewMovies={setNewMovies} movies={movies}/>
+    <SearchBar searchText={searchText} setSearchText={setSearchText} newMovies={newMovies} setNewMovies={setNewMovies}/>
+    <MovieList movies={movies} searchText={searchText} newMovies={newMovies}/>
+  </div>
+  )
+}
+
+
 
 export default App;
